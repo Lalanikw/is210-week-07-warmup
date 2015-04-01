3171f8ba3312db46d3d9b49d672d4bbdcddac4bc
Lalani Werake <lalanikw@gmail.com>
2015-04-01 23:34:28 +0000

    task_01.py

--- task_01.py
+++ task_01.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""lists and tuples"""
+"""creating lists and tuples."""
 
 ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
             'Carbon')
 
-OPERATIONS_ORDER = ['Parenthesis', 'Exponents', 'Multiplication', 'Division',
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',
                     'Addition', 'Subtraction']
